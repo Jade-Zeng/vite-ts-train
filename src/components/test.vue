f8d7e609de42474a46fadbac400ffab2e2608820
jade <jade.zeng@d1m.cn>
2021-05-24 16:40:00 +0800

    2021年05月24日16:39:59

---
+++ src/components/test.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>测试{{count}}</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    export default defineComponent({
+        data () {
+            return {
+                count: 10000
+            }
+        },
+        mounted() {
+            this.test()
+        },
+        methods: {
+            test () {
+                interface Card {
+                    suit: string;
+                    card: number;
+                }
+                interface Deck {
+                    suits: string[];
+                    cards: number[];
+                    createCardPicker(this: Deck): () => Card;
+                }
+                let deck: Deck = {
+                    suits: ["hearts", "spades", "clubs", "diamonds"],
+                    cards: Array(52),
+                    createCardPicker: function(this: Deck) {
+                        return () => {
+                            let pickedCard = Math.floor(Math.random() * 52);
+                            let pickedSuit = Math.floor(pickedCard / 13);
+
+                            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+                        }
+                    }
+                }
+
+                let cardPicker = deck.createCardPicker();
+                let pickedCard = cardPicker();
+
+                alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+            }
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
